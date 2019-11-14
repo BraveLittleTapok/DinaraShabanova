473676abc49885d462392b905f969e3fd83386a9
BraveLittleTapok <dinara.shabanova@yandex.ru>
2019-11-14 22:53:09 +0300

    Initial commit

---
+++ src/test/java/homework_1/dataproviders/DivideDataProvider.java
@@ -0,0 +1,21 @@
+package homework_1.dataproviders;
+
+import org.testng.annotations.DataProvider;
+
+import static homework_1.tests.Constants.LONG_MAX;
+import static homework_1.tests.Constants.LONG_MIN;
+
+/**
+ * Created by dinar on 14.11.2019.
+ */
+public class DivideDataProvider {
+    @DataProvider(name = "DivideDataProviderNoCheckingDivideByZero")
+    public Object[][] divideDataProviderNoCheckDivZero() {
+        return new Object[][]{
+                {1, 1, 1},
+                {0, LONG_MIN, 0},
+                {628, 25, 25},
+                {0, LONG_MAX, 0}
+        };
+    }
+}
