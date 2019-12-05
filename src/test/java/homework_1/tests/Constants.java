3e5c8558fbf28a99d99eaa18e4b4419b5f75ea8c
BraveLittleTapok <dinara.shabanova@yandex.ru>
2019-12-05 20:27:46 +0300

    trying change pom

--- src/test/java/homework_1/tests/Constants.java
+++
@@ -1,9 +0,0 @@
-package homework_1.tests;
-
-/**
- * Created by dinar on 14.11.2019.
- */
-public class Constants {
-    public static final long LONG_MAX = 9223372036854775807l;
-    public static final long LONG_MIN = -9223372036854775808l;
-}
